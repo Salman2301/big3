feaf3de992a7071cffbeb579cfeff89471bd9796
Salman <salmanhammed77@gmail.com>
2024-05-07 05:42:17 +0530

    add chat ui

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -19,7 +19,40 @@
 		</div>
 	</div>
 
-	<div class="sidebar-content"></div>
+	<div class="sidebar-content">
+		<h1>Chat</h1>
+		<div class="chat-content">
+			<div class="chat-message">
+				
+				<div class="chat-message-content">
+					<span class="chat-message-content-team">
+						username
+					</span>
+					This is the content of the chat
+				</div>
+
+				<div class="chat-message-content">
+					<span class="chat-message-content-team team-kdot">
+						username
+					</span>
+					This is the content of the chat
+				</div>
+				
+
+			</div>
+
+			<div class="chat-input">
+				<textarea>
+
+				</textarea>
+				<button>
+					<span></span>
+					Send
+				</button>
+			</div>
+		</div>
+	</div>
+
 </div>
 
 <style lang="postcss">
@@ -57,5 +90,47 @@
 	.detail-vote {
 		@apply flex justify-center flex-col items-center w-full;
 		@apply gap-8;
+	}
+
+	.sidebar-content h1 {
+		@apply text-2xl text-center;
+		padding: 10px;
+	}
+
+	.chat-content {
+		@apply flex flex-col;
+		@apply justify-between items-center;
+		height: calc(100vh - 60px);
+	}
+
+	.chat-message {
+		width: 100%;
+		padding: 0px 10px;
+	}
+
+	.chat-message-content-team {
+		@apply  font-bold text-drizzy;
+	}
+
+	.team-kdot {
+		@apply text-kdot;
+	}
+	.chat-message-content-team,
+	.chat-message-content {
+		font-size: 14px;
+	}
+
+	.chat-input {
+		@apply flex items-center flex-col;
+		width: 230px;
+	}
+
+	.chat-input textarea {
+		width: 100%;
+		background-color: rgba( 255, 255, 255, 0.3 );
+		border-radius: 6px;
+	}
+	.chat-input button {
+		align-self: flex-end;
 	}
 </style>
